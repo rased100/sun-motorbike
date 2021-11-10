5bbe95316d6581a3d3705c4036674ea27cb34201
rased100 <rased100@gmail.com>
2021-11-10 15:05:01 +0600

    login page running login data

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,21 +1,35 @@
 import { Button, TextField, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Login = () => {
+    const [loginData, setLoginData] = useState({});
+
+    const handleOnChange = e => {
+        const field = e.target.name;
+        const value = e.target.value;
+        console.log(field, value)
+        const newLoginData = { ...loginData };
+        newLoginData[field] = value;
+        setLoginData(newLoginData);
+    }
+
+    const handleLoginSubmit = e => {
+        alert('hello')
+        e.preventDefault();
+    }
     return (
         <Box sx={{ mt: 8 }} xs={12} md={6}>
             <Typography variant="body1" gutterBottom>Login</Typography>
-            {/* <form onSubmit={handleLoginSubmit}> */}
-            <form>
+            <form onSubmit={handleLoginSubmit}>
                 <TextField
                     sx={{ width: '50%', m: 1 }}
                     id="standard-basic"
                     label="Your Email"
                     type="email"
                     name="email"
-                    // onChange={handleOnChange}
+                    onChange={handleOnChange}
                     variant="standard" />
                 <TextField
                     sx={{ width: '50%', m: 1 }}
@@ -23,7 +37,7 @@ const Login = () => {
                     label="Your Password"
                     type="password"
                     name="password"
-                    // onChange={handleOnChange}
+                    onChange={handleOnChange}
                     variant="standard" /> <br />
 
                 <Button sx={{ width: '25%', m: 1 }} type="submit" variant="contained">Login</Button><br />
