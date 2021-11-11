3d97962d025783784b654af30de30ee389a72930
rased100 <rased100@gmail.com>
2021-11-11 21:54:46 +0600

    product with cardmedia

--- src/Pages/Product/Product.js
+++ src/Pages/Product/Product.js
@@ -1,12 +1,28 @@
-import { Grid } from '@mui/material';
+import { CardContent, CardMedia, Grid, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Product = (props) => {
-    const { name, img } = props.product;
+    const { name, img, description, _p_id } = props.product;
+    const url = `/product/${_p_id}`;
     return (
         <Grid item xs={2} sm={4} md={4} >
-            <h2>{name}</h2>
-            <img width='100%' src={img} alt="" />
+            <CardMedia
+                component="img"
+                style={{ width: '100%', margin: 'auto' }}
+                image={img}
+                alt="green iguana"
+            />
+            <CardContent>
+                <Typography variant="h5" component="div">
+                    {name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {description}
+                </Typography>
+            </CardContent>
+            <Link to={url}><button className="btn btn-primary mb-3">Book Now</button></Link>
         </Grid>
     );
 };
--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,4 +1,4 @@
-import { Grid } from '@mui/material';
+import { Container, Grid } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
 import useAuth from '../../hooks/useAuth';
@@ -7,13 +7,17 @@ import Product from '../Product/Product';
 const Products = () => {
     const { products } = useAuth();
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                {
-                    products.slice(0, 6).map(product => <Product product={product}></Product>)
-                }
-            </Grid>
-        </Box>
+        <Container style={{
+            margin: '50px auto'
+        }}>
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                    {
+                        products.slice(0, 6).map(product => <Product product={product}></Product>)
+                    }
+                </Grid>
+            </Box>
+        </Container >
     );
 };
 
