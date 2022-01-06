d678fd151fce699dcaa9d8bab216c1362a812745
rased100 <rased100@gmail.com>
2022-01-06 12:26:34 +0600

    user comment update

--- src/Pages/Review/Comment/Comment.js
+++ src/Pages/Review/Comment/Comment.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Comment.css';
 
 const Comment = (props) => {
     const { review } = props.comment;
     return (
         <div className="bg-white m-1">
-            <p className="p-3 text-start">{review}</p>
+            <p className="p-3 text-start">User Comment: <span className='p-1 comment'>{review}</span></p>
         </div>
     );
 };
